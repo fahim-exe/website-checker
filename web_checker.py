5b9f0162c587e2d7704226f80f191054e8811b22
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2023-09-07 22:08:45 +0600

    added more functions

--- web_checker.py
+++ web_checker.py
@@ -7,11 +7,6 @@ from fake_useragent import UserAgent
 from http import HTTPStatus
 
 
-def get_user_agent():
-    ua = UserAgent()
-    return ua.firefox
-
-
 def generate_csv():
     print("Please enter your website link. enter 'q' / 'Q' to end the entry.")
     with open("new_list.csv", "w", newline="") as file:
@@ -20,16 +15,66 @@ def generate_csv():
         writer.writerow(['SNo', 'Website-Address'])
 
         while True:
+            
             user_input = input(f"Enter {slno} number website:")
             if user_input.lower()=="q":
                 break
+            if "https://" not in user_input :
+                if "http://" not in user_input:
+                    user_input = "https://"+user_input
+                else:
+                    user_input = user_input[:4]+ "s" +user_input[4:]
+            
 
             writer.writerow([str(slno), user_input])
             slno+=1
 
-    output = csv.reader(file, delimiter=" ")
-    for row in output:
-        print(", ".join(row))
+        return
+
+
+def get_user_agent():
+    ua = UserAgent()
+    return ua.firefox
+
+
+def get_status_des(status_code):
+    for value in HTTPStatus:
+        if value == status_code:
+            description = f"({value} {value.name}), {value.descrition}"
+
+    return "(???) Unknown Status Code!!!"
+
+
+
+    
+def get_websites(csv_path, choice):
+    websites = []
+
+    if choice==1:
+        with open(csv_path, "r") as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                if "address" in row[1].lower():
+                    continue
+                websites.append(row[1])
+
+    return websites
+
+
+def check_website(web_link:str, user_agent):
+    try:
+        code = int(requests.get(web_link, headers={"User-Agent":user_agent}).status_code)
+        print(web_link, get_status_des(code))
+
+    except Exception:
+        print(f"Could not get information for website: {web_link}")
+
+def get_user_agent():
+    ua = UserAgent()
+
+
+
 
 
 
@@ -42,6 +87,14 @@ def main():
         num = int(input("Please enter your choice: "))
         if num==1:
             new_csv = generate_csv()
+            websites = get_websites("new_list.csv", 1)
+            print(websites)
+
+
+
+        if num==2:
+            pass
+
 
     except ValueError:
         print("Please enter a valide selection")
